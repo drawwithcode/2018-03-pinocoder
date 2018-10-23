b1d4ec48f35c87b6fa225fdd8af56279f7f560e0
pinocoder <41514345+pinocoder@users.noreply.github.com>
2018-10-23 20:48:42 +0200

    recupero consegna_3

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
-function preload(){
-  // put preload code here
-}
+let angle = 0;
 
 function setup() {
-  // put setup code here
+  createCanvas(800, 800);
+  angleMode(DEGREES);
+
 }
 
-function draw() {
-  // put drawing code here
-}
+  function draw() {
+    for (var x = 25; x < 400; x += 25) {
+      for (var y = 25; y < 400; y += 25) {
+        stroke(0);
+
+// scacchiera
+        if (x % 2 == 0 && y%2 !=0) {
+        fill(0);
+        }
+        else if (x % 2 != 0 && y%2 ==0) {
+        fill(0);
+        }
+        else {
+          fill (255);
+        }
+        rect(x, y, 25, 25);
+      }
+    }
+
+  }
